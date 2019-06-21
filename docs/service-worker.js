834b2a4eb3e777c04829176317638e63acae242a
Matt Reimer <matt.reimer@gmail.com>
2019-06-21 12:16:20 -0700

    new build

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/photomon_v0/precache-manifest.ee86c6c1bff6f97417ec5119d92480cd.js"
+  "/photomon_v0/precache-manifest.085166c7ab6e28aaddd5c9bc8c2748bd.js"
 );
 
 workbox.clientsClaim();
